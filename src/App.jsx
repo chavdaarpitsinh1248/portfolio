9d56752dc773acf3a5ec51547dd0864f41cbd603
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2026-02-04 17:01:20 +0530

    small fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
-import Projects from "./pages/Projects";
+import Projects from "./components/Projects";
 import Contact from "./pages/Contant";
 
 function App() {
--- src/pages/Projects.jsx
+++
@@ -1,9 +0,0 @@
-function Projects() {
-  return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold">Projects</h1>
-    </div>
-  );
-}
-
-export default Projects;
